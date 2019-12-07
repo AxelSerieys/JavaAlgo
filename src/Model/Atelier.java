428b27134f63eff803b45f91e882ddff49bd80d5
natha <natha@LAPTOP-AJGL5SGB>
2019-12-07 14:34:40 +0100

    Ajout Atelier

---
+++ src/Model/Atelier.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.util.List;
+
+/**
+ * Atelier contenant une chaine de production
+ * @author nathan
+ * Creer le: 
+ */
+public class Atelier {
+	
+	private List<ElementEspace> element;
+}
