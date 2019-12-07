1d25b42e7a5cd7eca03285c53b28081660c4df4b
Victor <Victor@Asus-Victor>
2019-12-07 18:36:59 +0100

    Classe ElementEspace

---
+++ src/Model/ElementEspace.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class ElementEspace {
+	private int posX;
+	private int posY;
+	
+	//Fonction SetPosition(int x, int y)?
+	
+	public void SetPositionX(int x) {
+		this.posX=x;
+	}
+	
+	public void SetPositionY(int y) {
+		this.posY=y;
+	}
+	
+	public int getPositionX() {
+		return posX;
+	}
+
+	public int getPositionY() {
+		return posY;
+	}
+}
