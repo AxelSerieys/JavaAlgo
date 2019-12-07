a12fe7c56218cb5a2ebd2c2b3831f888c19e0516
natha <natha@LAPTOP-AJGL5SGB>
2019-12-07 14:40:05 +0100

    Modif Atelier

--- src/Model/Atelier.java
+++ src/Model/Atelier.java
@@ -5,9 +5,22 @@ import java.util.List;
 /**
  * Atelier contenant une chaine de production
  * @author nathan
- * Creer le: 
+ * Creer le: 07/12/2019
  */
 public class Atelier {
 	
+	/** Attribut Atelier: Liste element **/
 	private List<ElementEspace> element;
+
+	/** Getter d'un element **/
+	public List<ElementEspace> getElement() {
+		return element;
+	}
+
+	/** Setter d'un element **/
+	public void setElement(List<ElementEspace> element) {
+		this.element = element;
+	}
+	
+	
 }
