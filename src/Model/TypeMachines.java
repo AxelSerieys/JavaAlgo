e5303f0b484e749cb3d01c8d4df86afbeb5bd5c2
Victor <Victor@Asus-Victor>
2019-12-10 00:06:14 +0100

    Remplacement de l'interface TypeMachine par une classe TypeMachines pour que l'utilisateur puisse creer ses propres types

---
+++ src/Model/TypeMachines.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class TypeMachines {
+	private String type; 
+	
+	public TypeMachines(String type) {
+		this.type=type;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+
+}
