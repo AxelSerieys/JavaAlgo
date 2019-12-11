5316b8f1ed888d1ea21ba77092da01183fd2bbef
Axel Sérieys <serieyaxel@gmail.com>
2019-12-11 10:39:59 +0100

    Ajout du test de convoyeur

---
+++ src/Test/ConvoyeurTest.java
@@ -0,0 +1,74 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Model.Convoyeur;
+import Model.Produit;
+
+class ConvoyeurTest {
+
+	/**
+	 * On teste la méthode getCapacité après la création d'un convoyeur
+	 */
+	@Test
+	void testGetCapacite() {
+		int capacite=32;
+		Convoyeur c = new Convoyeur(1, 2, capacite, 1);
+		
+		assertEquals(capacite, c.getCapacite());
+	}
+	
+	/**
+	 * On teste la méthode getVitesse après la création d'un convoyeur
+	 */
+	@Test
+	void testGetVitesse() {
+		int vitesse = 3;
+		Convoyeur c = new Convoyeur(1, 1, 32, vitesse);
+		
+		assertEquals(vitesse, c.getVitesseDeplacement());
+	}
+	
+	/**
+	 * On teste la mise à jour du produit stocké dans un convoyeur
+	 */
+	@Test
+	void testSetProduit() {
+		Convoyeur c = new Convoyeur(1, 2, 32, 3);
+		Produit p = new Produit("type", 1);
+		Produit p2 = new Produit("type", 2);
+		
+		c.setProduit(p);
+		assertEquals(p, c.getProduit());
+		
+		c.setProduit(p2);
+		assertEquals(p2, c.getProduit());
+	}
+	
+	/**
+	 * On teste la méthode setVitesseDeplacement qui met à jour la vitesse de déplacement d'un convoyeur
+	 */
+	@Test
+	void testSetVitesseDeplacement() {
+		int vitesse=32;
+		Convoyeur c = new Convoyeur(1, 2, 3, 4);
+		c.setVitesseDeplacement(vitesse);
+		
+		assertEquals(vitesse, c.getVitesseDeplacement());
+	}
+	
+	/**
+	 * On teste la méthode setCapacite qui met à jour la capacité maximale d'un convoyeur
+	 */
+	@Test
+	void testSetCapacite() {
+		int capacite=12;
+		Convoyeur c = new Convoyeur(1, 2, 3, 4);
+		c.setCapacite(capacite);
+		
+		assertEquals(capacite, c.getCapacite());
+	}
+
+}
