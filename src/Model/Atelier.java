f9e3e23c15e84652d17a27ad47335a194f9a7958
Axel Sérieys <serieyaxel@gmail.com>
2019-12-11 14:48:40 +0100

    Suppression du constructeur vide d'atelier et ajout javadoc

--- src/Model/Atelier.java
+++ src/Model/Atelier.java
@@ -21,13 +21,6 @@ public class Atelier {
 	private Stock stockPF;
 	private Object [][] tabPdts;
 	
-	/**
-	 * Constructeur d'un Atelier
-	 */
-	public Atelier() {
-
-	}
-	
 	/** 
 	 * Configuration des îlots de l'atelier et de leur opération 
 	 */
@@ -118,7 +111,11 @@ public class Atelier {
 		}
 	}
 	
-	public Object[][] nbProduitsAFabriquer() {
+	/**
+	 * iste des produits à fabriquer
+	 * @return Object[][] nom de la gamme, nombre de produits dans cette gamme
+	 */
+	public Object[][] nbProduitsAFabriquer() { //Nom de la gamme, nbre de produit de cette gamme
 		tabPdts = new Object [listeGammes.size()][2];
 		for (int i=0; i<listeGammes.size(); i++) {
 			tabPdts[i][0]=listeGammes.get(i).getNom();
@@ -128,10 +125,18 @@ public class Atelier {
 		return tabPdts;
 	}
 	
+	/**
+	 * Renvoie le temps de traitement d'une pièce dans un îlot donné
+	 * @param ilot l'îlot dont la durée sera renvoyée
+	 * @return la durée de traitement d'une pièce dans un îlot
+	 */
 	public int tpsTraitementPiece(Ilot ilot) {
 		return ilot.getOperation().getTempsRealisation();
 	}
 	
+	/**
+	 * Ajoute des produits dans l'îlot
+	 */
 	public void ajoutPdtIlot() {
 		Gamme g = getGammePdt(getPdtTopStock());
 		int numPhase = getPdtTopStock().getNumeroPhase();
@@ -140,18 +145,50 @@ public class Atelier {
 
 	}
 	
+	/**
+	 * Ajoute des produits dans un îlot donné
+	 * @param ilot
+	 */
 	public void ajoutPdt(Ilot ilot) {
 		ilot.ajoutPdt(getPdtTopStock());
 	}
 	
+	/**
+	 * Retourne le premier élément du stock de matières premières
+	 * @return Produit le premier élément de la liste
+	 */
 	public Produit getPdtTopStock() {
-		return getStockMP().getProduits().get(getSizeStockMP()-1);
+		List<Produit> list = getStockMP().getProduits();
+		return list.get(getSizeStockMP()-1);
 	}
 	
+	/**
+	 * Retourne le premier Îlot dont le typeMachine est équivalent à celui passé en paramètre
+	 * @param type le type des machines
+	 * @return un Îlot
+	 */
+	public Ilot getIlot(String type) {
+		for(Ilot i : listeIlots)
+			if(i.getType() == type)
+				return i;
+		
+		return null;
+	}
+	
+	/**
+	 * Retourne une îlot à un index donné
+	 * @param i l'index de l'ilot
+	 * @return l'îlot à l'index donné
+	 */
 	public Ilot getIlot(int i) {
 		return listeIlots.get(i);
 	}
 	
+	/**
+	 * Retourne l'ilot dont l'opération est passée en paramètre
+	 * @param op l'opération
+	 * @return l'ilot dont l'opération est passée en paramètre
+	 */
 	public Ilot getIlot (Operation op) {
 		int j=-1;
 		for(int i=0; i<listeIlots.size();i++) {
@@ -165,6 +202,11 @@ public class Atelier {
 		return listeIlots.get(j);
 	}
 	
+	/**
+	 * Retourne la gamme d'un produit donné
+	 * @param pdt le produit
+	 * @return Gamme , sa gamme
+	 */
 	public Gamme getGammePdt(Produit pdt) {
 		int j=-1;
 		for(int i=0; i<listeGammes.size(); i++) {
@@ -178,44 +220,86 @@ public class Atelier {
 		return listeGammes.get(j);
 	}
 	
+	/**
+	 * Retourne la gamme à un indice donné dans la liste de gammes interne à l'atelier
+	 * @param i l'indice de la game
+	 * @return Gamme, la gamme à l'indice donné
+	 */
 	public Gamme getGamme(int i) {
 		return listeGammes.get(i);
 	}
 	
+	/**
+	 * Retourne le nombre d'Îlots dans l'atelier
+	 * @return int le nombre d'îlots
+	 */
 	public int getNbIlots() {
 		return listeIlots.size();
 	}
 	
+	/**
+	 * Retourne le nombre de gammes
+	 * @return int le nombre de gammes
+	 */
 	public int getNbGammes() {
 		return listeGammes.size();
 	}
 	
+	/**
+	 * Ajoute des produits dans le stock des matières premières
+	 * @param pdt : le nomnre de produits à ajouter
+	 * @param g la gamme dont les produits seront ajoutés
+	 */
 	public void setStockMP(int pdt, Gamme g) {
 		for(int i=0; i<pdt; i++) {
 			this.stockMP.addProduit(new Produit(g.getNom(),0));
 		}
 	}
 	
+	/**
+	 * Enlève un produit du stock de matières premieres
+	 * @param pdt l'indice du produit dans le stock de matières premières
+	 */
 	public void remStockMP(int pdt) {
 		this.stockMP.removeProduit(pdt);
 	}
 	
+	/**
+	 * Retourne le stock de matières premières
+	 * @return Stock, le stock de matières premières
+	 */
 	public Stock getStockMP() {
 		return this.stockMP;
 	}
 	
+	/**
+	 * Retourne la taille du stock de matières premières
+	 * @return int la taille du stock
+	 */
 	public int getSizeStockMP() {
 		return this.stockMP.getProduits().size();
 	}
 	
+	/**
+	 * Ajoute un produit dans le stock des produits finis
+	 * @param pdt le produit à ajouter
+	 */
 	public void addStockPF(Produit pdt) {
 		this.stockPF.addProduit(pdt);
 	}
 	
+	/**
+	 * Retourne le stock des produits finis
+	 * @return Stock, le stock des produits finis
+	 */
 	public Stock getStockPF() {
 		return this.stockPF;
 	}
 	
+	/**
+	 * Retourne la taille du stock des produits finis
+	 * @return la taille du stock
+	 */
 	public int getSizeStockPF() {
 		return this.stockPF.getProduits().size();
 	}
